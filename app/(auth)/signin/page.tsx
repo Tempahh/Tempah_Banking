3cd381518670df862f229637904712fcda748a26
Prosper <prosperorjiogo1999@gmail.com>
2024-08-13 12:27:48 +0100

    implement initial file and folder structure for app

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signin = () => {
+  return (
+    <div>
+      SignIn
+    </div>
+  )
+}
+
+export default signin
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const signup = () => {
+  return (
+    <div>
+      SignUp
+    </div>
+  )
+}
+
+export default signup
